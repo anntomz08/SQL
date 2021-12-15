d599197e6ceef435338d872bb4b5561366ca3e30
Anna Thomas <70516088+anntomz08@users.noreply.github.com>
2021-12-15 15:01:26 +0530

    New File

--- Exp2:Test.sql
+++ Exp2:Test.sql
@@ -1,8 +1,8 @@
-//UPDATE latefees which has been incremented by 10%
+--UPDATE latefees which has been incremented by 10%
 
 UPDATE BOOK_RETURN SET latefees=latefees+ (0.1*latefees);
 
-//DISPLAY the AUTHORS
+--DISPLAY the AUTHORS
 
 SELECT * FROM author;
 
